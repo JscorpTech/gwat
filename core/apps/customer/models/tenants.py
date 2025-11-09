1a7dd210aefb2d715a4b27c64aeec94a4aff7d47
A'zamov Samandar <admin@jscorp.uz>
2025-11-10 01:55:31 +0500

    update

--- core/apps/customer/models/tenants.py
+++ core/apps/customer/models/tenants.py
@@ -1,12 +1,11 @@
-from django.db import models, connections
-from django_tenants.models import TenantMixin, DomainMixin, post_schema_sync, schema_needs_to_be_sync
-from django_tenants.utils import get_tenant_database_alias, schema_exists
-
+from django.db import models
+from django_tenants.models import TenantMixin, DomainMixin
 
 class Client(TenantMixin):
     name = models.CharField(max_length=100)
     paid_until = models.DateField()
     on_trial = models.BooleanField()
+    logo = models.FileField(upload_to="logo/", null=True, blank=True)
     created_on = models.DateField(auto_now_add=True)
 
     auto_create_schema = True
--- resources/scripts/entrypoint.sh
+++ resources/scripts/entrypoint.sh
@@ -9,6 +9,9 @@ python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
 
 # python3 manage.py events &
-uvicorn config.asgi:application --host 0.0.0.0 --port 8000 --reload --reload-dir core --reload-dir config
+# uvicorn config.asgi:application --host 0.0.0.0 --port 8000 --reload --reload-dir core --reload-dir config
+export PYDEVD_DISABLE_FILE_VALIDATION=1
+
+python3 -m debugpy --listen 0.0.0.0:5678 manage.py runserver 0.0.0.0:8000
 
 exit $?
