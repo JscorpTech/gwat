2e9c5d0e8ffa71e02bb6475b3ab28f3559d5344f
A'zamov Samandar <admin@jscorp.uz>
2025-11-25 16:00:51 +0500

    fix typo

--- config/asgi.py
+++ config/asgi.py
@@ -1,15 +1,17 @@
+# type: ignore
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-
-from config.env import env
-from core.apps.websocket.urls import websocket_urlpatterns
-from core.apps.websocket.middlewares import JWTAuthMiddlewareStack
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", env("DJANGO_SETTINGS_MODULE"))
 
 django_asgi_app = get_asgi_application()
+from channels.routing import ProtocolTypeRouter, URLRouter  # noqa
+from channels.security.websocket import AllowedHostsOriginValidator  # noqa
+
+from config.env import env  # noqa
+from core.apps.websocket.urls import websocket_urlpatterns  # noqa
+from core.apps.websocket.middlewares import JWTAuthMiddlewareStack  # noqa
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", env("DJANGO_SETTINGS_MODULE"))  # noqa
+
 
 application = ProtocolTypeRouter(
     {
